efd341e1fe532be25cb32a4aacb2a13f31c97fd5
Marcus Bitzl <marcus@bitzl.com>
2016-03-05 14:53:00 +0100

    Added first tests.

---
+++ test/transform_test.js
@@ -0,0 +1,34 @@
+var expect = require('chai').expect
+    HunspellLineToWord = require('../lib/transformer').HunspellLineToWord,
+    StreamTest = require('streamtest')['v2'];
+
+describe('HunspellLineToWord', function() {
+   describe('extractWordFromHunspellLine()', function() {
+     context('when line is given', function() {
+       it('should remove anything after the first "/".', function(done) {
+         StreamTest.fromChunks(['abc/23,23/2192a/asd/212'])
+          .pipe(new HunspellLineToWord())
+          .pipe(StreamTest.toText(function(error, text){
+              expect(text).to.equal('abc');
+              done();
+          }));
+       });
+       it('should remove any whitespace before and after.', function(done) {
+         StreamTest.fromChunks(['  abc /23 '])
+          .pipe(new HunspellLineToWord())
+          .pipe(StreamTest.toText(function(error, text){
+              expect(text).to.equal('abc');
+              done();
+          }));
+       });
+       it('should work with plain words without metadata.', function(done) {
+         StreamTest.fromChunks(['abc'])
+          .pipe(new HunspellLineToWord())
+          .pipe(StreamTest.toText(function(error, text){
+              expect(text).to.equal('abc');
+              done();
+          }));
+       });
+     });
+   });
+ });
